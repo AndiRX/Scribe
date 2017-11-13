a2e029f72d36543599b566279ff28cd9d55f2685
Petr <petr@petrs-air.localdomain>
2017-11-14 00:59:17 +0100

    done, need to be tested on iPhone

--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcriptTextField: UITextView!
+    
+    var audioPlayer: AVAudioPlayer!
+    var togglePlayBtn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        activitySpinner.isHidden = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
     }
-
-
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        
+                    } catch {
+                        print("error")
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) {
+                        (result, error) in
+                        if let error = error {
+                            print("there was an error \(error)")
+                        } else {
+                            print()
+                            
+                        }
+                    }
+                }
+                
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func playBtnPressed(_ sender: Any) {
+        if togglePlayBtn == false {
+            togglePlayBtn = true
+            activitySpinner.isHidden = false
+            activitySpinner.startAnimating()
+            requestSpeechAuth()
+            
+        } else {
+            togglePlayBtn = false
+//            activitySpinner.isHidden = true
+//            activitySpinner.stopAnimating()
+            audioPlayer.stop()
+            
+        }
+    }
+    
+    
 }
 
